c01724ac8367f1ea720d3c639bcc66701b1198ed
ruists <clovias@gmail.com>
2021-02-12 23:17:10 +0000

    Data is now updated in case it already exists in the db Can now be used to update a database populated by this script

--- parser.py
+++ parser.py
@@ -12,11 +12,13 @@ import html
 
 session = requests.Session()
 
+
 class Data:
     def __init__(self, config):
         connUrlTemplate = "mongodb+srv://{user}:{password}@trainingcluster.3uayb.mongodb.net/{dbname}?retryWrites=true&w=majority"
         db_name = config['db_name']
-        connUrl = connUrlTemplate.format(user = config['user'], password = config['password'], dbname = db_name)
+        connUrl = connUrlTemplate.format(
+            user=config['user'], password=config['password'], dbname=db_name)
         self.client = pymongo.MongoClient(connUrl)
         self.db = self.client[db_name]
         self.getBaseData()
@@ -32,13 +34,14 @@ class Data:
         self.weaponTypes = self.db['WeaponType']
 
 
-def sessionGet(url, params = {}):
-    request = session.get(url = url, params = params)
+def sessionGet(url, params={}):
+    request = session.get(url=url, params=params)
 
     if request.status_code != 200:
         print('Got status code', request.status_code, 'for', url)
-  
+
     return request
+
 
 def getConfigData():
     configParser = configparser.ConfigParser()
@@ -50,7 +53,8 @@ def getConfigData():
 
     return config
 
-def parseBaseData(table, fields): 
+
+def parseBaseData(table, fields):
     results = []
     ids = set()
     limit = 500
@@ -58,8 +62,8 @@ def parseBaseData(table, fields):
 
     while True:
         request = sessionGet(
-            url = 'https://gbf.wiki/api.php',
-            params = {
+            url='https://gbf.wiki/api.php',
+            params={
                 'action': 'cargoquery',
                 'tables': table,
                 'fields': fields,
@@ -84,56 +88,62 @@ def parseBaseData(table, fields):
             if not unique in ids:
                 results += [obj]
                 ids.add(unique)
-        
+
         if len(request_json['cargoquery']) != limit:
             break
 
     return results
 
+
 def updateBaseSummons(data, summons):
-    to_insert = []
     for summon in summons:
-        #ignore Spearsting
+        # ignore Spearsting
         if summon['name'].strip() == "Spearsting":
             continue
 
-        element = data.elements.find_one({"name":summon['element'].capitalize().strip()})
-        rarity = data.rarities.find_one({"name":summon['rarity'].strip()})
+        element = data.elements.find_one(
+            {"name": summon['element'].capitalize().strip()})
+        rarity = data.rarities.find_one({"name": summon['rarity'].strip()})
 
-        #unescape name twice to avoid accumulated html encodings (ampersand and apostrophes mostly)
+        # unescape name twice to avoid accumulated html encodings (ampersand and apostrophes mostly)
         s = {
-            "name":html.unescape(html.unescape(summon['name'].strip())),
+            "name": html.unescape(html.unescape(summon['name'].strip())),
             "maxUncap": int(summon['evo max'].strip()),
             "baseUncap": int(summon['evo base'].strip()),
-            "imgUrl": data.summonImageTemplate.format(summonId = summon['id']),
-            "element":element['_id'],
-            "rarity":rarity['_id']
+            "imgUrl": data.summonImageTemplate.format(summonId=summon['id']),
+            "element": element['_id'],
+            "rarity": rarity['_id']
         }
-        to_insert += [s]
-    result = data.db['BaseSummon'].insert_many(to_insert)
+
+        data.db['BaseSummon'].update_one({'name': s['name'], 'element': s['element'], 'rarity': s['rarity']}, {
+                                         '$set': {'maxUncap': s['maxUncap'], 'baseUncap': s['baseUncap'], 'imgUrl': s['imgUrl']}}, upsert=True)
+
 
 def updateBaseWeapons(data, weapons):
-    to_insert = []
     to_ignore = [
-        "Buster Sword", 
-        "Serpentine", 
-        "Kukri", 
-        "Tizona", 
-        "Panabas", 
-        "Bhuj", 
-        "Jedburgh Axe", 
-        "Regulon", 
+        "Buster Sword",
+        "Serpentine",
+        "Kukri",
+        "Tizona",
+        "Panabas",
+        "Bhuj",
+        "Jedburgh Axe",
+        "Regulon",
         "Circle of Life and Death"
     ]
-    
-    for weapon in weapons: 
+
+    for weapon in weapons:
         weaponName = weapon['name'].strip()
         if weaponName in to_ignore:
             continue
-        
-        element = data.elements.find_one({"name":weapon['element'].capitalize().strip()})
-        rarity = data.rarities.find_one({"name":weapon['rarity'].strip()})
-        wType = data.weaponTypes.find_one({"name":weapon['type'].capitalize().strip()})
+        if "(element)" in weaponName.lower():
+            continue
+
+        element = data.elements.find_one(
+            {"name": weapon['element'].capitalize().strip()})
+        rarity = data.rarities.find_one({"name": weapon['rarity'].strip()})
+        wType = data.weaponTypes.find_one(
+            {"name": weapon['type'].capitalize().strip()})
         if not wType:
             continue
         if not element:
@@ -143,68 +153,83 @@ def updateBaseWeapons(data, weapons):
             "name": html.unescape(html.unescape(weaponName)),
             "maxUncap": int(weapon['evo max']),
             "baseUncap": int(weapon['evo base']),
-            "imgUrl": data.weaponImageTemplate.format(weaponId = weapon['id']),
+            "imgUrl": data.weaponImageTemplate.format(weaponId=weapon['id']),
             "element": element['_id'],
             "rarity": rarity['_id'],
             "weaponType": wType['_id']
         }
-        to_insert += [w]
-    result = data.db['BaseWeapon'].insert_many(to_insert)
+
+        data.db['BaseWeapon'].update_one({'name': w['name'], 'element': w['element'], 'rarity': w['rarity']}, {
+                                         '$set': {'maxUncap': w['maxUncap'], 'baseUncap': w['baseUncap'], 'imgUrl': w['imgUrl'], 'weaponType': w['weaponType']}}, upsert=True)
+
 
 def updateBaseCharacters(data, characters):
-    to_insert = []
     for character in characters:
-        element = data.elements.find_one({"name":character['element'].capitalize().strip()})
-        rarity = data.rarities.find_one({"name":character['rarity'].upper().strip()})
-        style = data.styles.find_one({"name":character['type'].capitalize().strip()})
+        element = data.elements.find_one(
+            {"name": character['element'].capitalize().strip()})
+        rarity = data.rarities.find_one(
+            {"name": character['rarity'].upper().strip()})
+        style = data.styles.find_one(
+            {"name": character['type'].capitalize().strip()})
         race = []
         weapon = []
 
         rTokens = character['race'].split(',')
         for token in rTokens:
-            r = data.races.find_one({"name":token.strip().capitalize().strip()})
+            r = data.races.find_one(
+                {"name": token.strip().capitalize().strip()})
             race += [r['_id']]
         wTokens = character['weapon'].split(',')
         for token in wTokens:
-            w = data.weaponTypes.find_one({"name":token.strip().capitalize().strip()})
+            w = data.weaponTypes.find_one(
+                {"name": token.strip().capitalize().strip()})
             weapon += [w['_id']]
 
         c = {
             "name": character['name'].strip(),
             "maxUncap": character['max evo'],
-            "imgUrl": data.charaImageTemplate.format(characterId = character['id']),
+            "imgUrl": data.charaImageTemplate.format(characterId=character['id']),
             "race": race,
             "element": element['_id'],
             "rarity": rarity['_id'],
             "style": style['_id'],
             "weaponType": weapon
         }
-        to_insert += [c]
-    result = data.db['BaseCharacter'].insert_many(to_insert)
-    
+
+        data.db['BaseCharacter'].update_one({'name': c['name'], 'element': c['element'], 'rarity': c['rarity']}, {
+                                            '$set': {'maxUncap': c['maxUncap'], 'weaponType': c['weaponType'], 'imgUrl': c['imgUrl'], 'race': c['race'], 'style': c['style']}}, upsert=True)
+
+
 def setBaseSummons(data):
     print("Fetching summon data...")
-    summons = parseBaseData('summons','id,name,evo_base,evo_max,rarity,element')
+    summons = parseBaseData(
+        'summons', 'id,name,evo_base,evo_max,rarity,element')
     print("Preparing data and inserting in database...")
-    updateBaseSummons(data,summons)
+    updateBaseSummons(data, summons)
+
 
 def setBaseWeapons(data):
     print("Fetching weapon data...")
-    weapons = parseBaseData('weapons','id,name,evo_base,evo_max,rarity,element,type')
+    weapons = parseBaseData(
+        'weapons', 'id,name,evo_base,evo_max,rarity,element,type')
     print("Preparing data and inserting in database...")
     updateBaseWeapons(data, weapons)
 
+
 def setBaseCharacters(data):
     print("Fetching characters data...")
-    characters = parseBaseData('characters','id,name,max_evo,rarity,element,type,race,weapon')
+    characters = parseBaseData(
+        'characters', 'id,name,max_evo,rarity,element,type,race,weapon')
     print("Preparing data and inserting in database...")
     updateBaseCharacters(data, characters)
 
+
 def main():
     gbfData = Data(getConfigData())
-    #setBaseSummons(gbfData)
-    #setBaseWeapons(gbfData)
+    setBaseSummons(gbfData)
+    setBaseWeapons(gbfData)
     setBaseCharacters(gbfData)
+
 
 if __name__ == "__main__":
     main()
